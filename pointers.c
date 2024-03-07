34fb52c376b1ee450226a96cff3713ade685c3b5
Taha_Helmy <153909093+Thelmi@users.noreply.github.com>
2024-03-07 12:51:22 +0400

    initial stage

---
+++ pointers.c
@@ -0,0 +1,24 @@
+// Intro to pointers 
+// ***************************
+
+#include <stdio.h>
+int main()
+{
+// int a = 50;
+// int *p = &a;
+
+// printf("p: %p\n", p);	//address of variable a;
+// printf("a: %d\n", a); // value of variable a;
+// printf("&a: %p\n", &a); // address of variable a;
+// printf("&p: %p\n", &p);	// address of the pointer;
+// printf("*p: %d\n", *p);	// value of *p;
+// *p = 8;        // change value of a using the pointer (Dereferencing)
+// printf("*p: %d\n", *p);
+// printf("a: %d\n", a);
+
+// char c = 'a';
+// char *p = &c;
+// printf("%c\n",c);
+// printf("%c\n",*p);
+}
+
